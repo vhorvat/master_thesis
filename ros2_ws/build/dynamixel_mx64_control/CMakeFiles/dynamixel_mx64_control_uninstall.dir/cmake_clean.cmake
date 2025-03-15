161d3862c132b6263a21c94a15276f9fb2b8be0c
Viktor Horvat <vh51195@fer.hr>
2025-03-15 15:21:06 +0100

    code cleanup
    * removed build files, no need for them in the repo

--- ros2_ws/build/dynamixel_mx64_control/CMakeFiles/dynamixel_mx64_control_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/dynamixel_mx64_control_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dynamixel_mx64_control_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- ros2_ws/build/dynamixel_mx64_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vhorvat/Documents/master_thesis/ros2_ws/src/dynamixel_mx64_control
-# Build directory: /home/vhorvat/Documents/master_thesis/ros2_ws/build/dynamixel_mx64_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
